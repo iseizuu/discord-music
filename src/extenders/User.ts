fd23eee45dc276c3e3316fd9d538042c2c805707
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-11-02 17:34:14 +0700

    Update User.ts

--- src/extenders/User.ts
+++ src/extenders/User.ts
@@ -6,10 +6,10 @@ class MusicUser extends Structures.get("User") {
     }
 }
 
-Structures.extend("User", () => MusicUser);
-
 declare module "discord.js" {
     export interface User {
         isDev: boolean;
     }
 }
+
+Structures.extend("User", () => MusicUser);
