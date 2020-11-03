51b028d52fa851a81a5ed8e37cac3681cc2f93d2
Allvaa <alvamailindo@gmail.com>
2020-11-03 10:43:01 +0700

    use regex.exec instead of string.match and rename file

---
+++ src/utils/YouTubeSearch.ts
@@ -0,0 +1,39 @@
+import type { Client } from "discord.js";
+import VideoInfo from "../structures/VideoInfo";
+import Util from "./Util";
+
+export default class YouTubeSearch {
+    private readonly initialDataRegex = /(window\["ytInitialData"]|var ytInitialData)\s*=\s*(.*);/;
+    public constructor(public readonly client: Client) {}
+
+    public async load(query: string): Promise<VideoInfo[]> {
+        const { text } = await this.client.httpClient
+            .get("https://www.youtube.com/results")
+            .query("search_query", query);
+        return this.extractVideo(text);
+    }
+
+    private extractVideo(html: string): VideoInfo[] {
+        // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
+        const matched = this.initialDataRegex.exec(html)![2];
+        const result = JSON.parse(matched);
+        const videos = result
+            .contents
+            .twoColumnSearchResultsRenderer
+            .primaryContents
+            .sectionListRenderer
+            .contents[0]
+            .itemSectionRenderer
+            .contents;
+        return videos
+            .filter((x: any) => Boolean(x.videoRenderer))
+            .map((x: any) => this.extractData(x.videoRenderer)) as VideoInfo[];
+    }
+
+    private extractData(video: any): VideoInfo {
+        const title = video.title.runs[0].text;
+        const author = video.ownerText.runs[0].text;
+        const duration = video.lengthText ? Util.durationToMillis(video.lengthText.simpleText) : null;
+        return new VideoInfo(video.videoId, title, author, duration, `https://www.youtube.com/watch?v=${video?.videoId}`);
+    }
+}
