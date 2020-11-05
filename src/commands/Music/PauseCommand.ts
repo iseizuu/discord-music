db39ecbbc10056800d449bd8665ab613d7d0e362
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-11-05 18:11:35 +0700

    add skip, pause, resume commands

---
+++ src/commands/Music/PauseCommand.ts
@@ -0,0 +1,25 @@
+import type { Message } from "discord.js";
+import Command from "../../structures/Command";
+import { CommandConf } from "../../decorators";
+
+@CommandConf({
+    name: "pause",
+    aliases: [""],
+    description: "",
+    usage: "",
+    ownerOnly: false
+})
+export default class PauseCommand extends Command {
+    public async exec(msg: Message): Promise<Message | void> {
+        const voiceChannel = msg.member?.voice.channel;
+
+        if (!msg.guild?.music.current) return;
+        if (!voiceChannel) return msg.channel.send("You need to join voice channel first");
+        if (msg.guild?.music.current && voiceChannel.id !== msg.guild?.music.channel.voice?.id) {
+            return msg.channel.send(`I'am sorry, you need to be in **${
+                msg.guild?.music.channel.voice!.name}** voice channel`);
+        }
+        void msg.react("⏸");
+        msg.guild?.music.togglePause();
+    }
+}
---
+++ src/commands/Music/SkipCommand.ts
@@ -0,0 +1,26 @@
+import type { Message } from "discord.js";
+import Command from "../../structures/Command";
+import { CommandConf } from "../../decorators";
+
+@CommandConf({
+    name: "skip",
+    aliases: [""],
+    description: "",
+    usage: "",
+    ownerOnly: false
+})
+export default class SkipCommand extends Command {
+    public async exec(msg: Message): Promise<Message | void> {
+        const voiceChannel = msg.member?.voice.channel;
+        
+        if (!msg.guild?.music.current) return;
+        if (!voiceChannel) return msg.channel.send("You need to join voice channel first");
+        if (msg.guild?.music.current && voiceChannel.id !== msg.guild?.music.channel.voice?.id) {
+            return msg.channel.send(`I'am sorry, you need to be in **${
+                msg.guild?.music.channel.voice!.name}** voice channel`);
+        }
+
+        void msg.react("✅");
+        msg.guild?.music.skip();
+    }
+}
