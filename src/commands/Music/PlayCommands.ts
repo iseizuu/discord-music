daae4f3f416c0d1590901294e425b54e5e307e67
Allvaa <alvamailindo@gmail.com>
2020-11-06 22:04:47 +0700

    add support for playlist

--- src/commands/Music/PlayCommands.ts
+++ src/commands/Music/PlayCommands.ts
@@ -2,7 +2,6 @@ import type { Message, TextChannel } from "discord.js";
 import Command from "../../structures/Command";
 import { CommandConf } from "../../decorators";
 import Playlist from "../../structures/Playlist";
-import type VideoInfo from "../../structures/VideoInfo";
 
 @CommandConf({
     name: "play",
@@ -24,26 +23,38 @@ export default class PlayCommand extends Command {
         }
 
         const result = await msg.guild?.music.search(query);
-        let songs: VideoInfo[];
-        if (result instanceof Playlist) songs = result.videos;
-        else songs = result!;
-
-        if (!songs?.length) return msg.channel.send("Sorry, no results found");
-
-        if (msg.guild?.music.current) {
+        if (result instanceof Playlist) {
+            const songs = result.videos;
+            for (const song of songs) {
+                msg.guild?.music.add(song);
+            }
             await msg.channel.send({
                 embed: {
                     color: this.client.config.color,
-                    description: `**[${songs[0].title}](${songs[0].url}) Added to queue**`
+                    description: `**\`${songs.length}\`songs added to queue**`
                 }
             });
+        } else {
+            const songs = result;
+            if (!songs?.length) return msg.channel.send("Sorry, no results found");
+
+            if (msg.guild?.music.current) {
+                await msg.channel.send({
+                    embed: {
+                        color: this.client.config.color,
+                        description: `**[${songs[0].title}](${songs[0].url}) Added to queue**`
+                    }
+                });
+            }
+
+            msg.guild?.music.add(songs[0], msg.author);
         }
 
-        msg.guild?.music.add(songs[0], msg.author);
         if (!msg.guild?.music.channel.voice) {
             await msg.guild?.music.join(msg.member!.voice.channel!, msg.channel as TextChannel);
             await msg.member?.guild.me?.voice.setSelfDeaf(true);
         }
+
         if (!msg.guild?.music.dispatcher) msg.guild?.music.start();
     }
 }
