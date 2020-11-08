c4f1d6afcc1ba2c63f591407723c7b440e43e39e
Allvaa <alvamailindo@gmail.com>
2020-11-08 14:25:21 +0700

    add nowplay command

---
+++ src/commands/Music/NowPlayingCommand.ts
@@ -0,0 +1,21 @@
+import type { Message } from "discord.js";
+import { CommandConf } from "../../decorators";
+import Command from "../../structures/Command";
+
+@CommandConf({
+    name: "nowplaying",
+    aliases: ["nowplay", "np"]
+})
+export default class NowPlayingCommand extends Command {
+    public async exec(msg: Message): Promise<void> {
+        if (!msg.guild?.music.current) return;
+        await msg.channel.send({
+            embed: {
+                color: this.client.config.color,
+                description: `**Now Playing: [${msg.guild?.music.current.info.title}](${msg.guild?.music.current.info.url}) [${
+                    this.client.util.parseDur(msg.guild.music.current.info.duration!)
+                }]**`
+            }
+        });
+    }
+}
