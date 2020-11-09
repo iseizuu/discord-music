ad1a7f2aacc7659dc2eb606d7ff61c07340a1535
Allvaa <alvamailindo@gmail.com>
2020-11-09 09:30:25 +0700

    biar bagus

--- src/commands/Music/QueueCommand.ts
+++ src/commands/Music/QueueCommand.ts
@@ -25,13 +25,18 @@ export default class QueueCommand extends Command {
         }
 
         const queue = guildMusic?.queue
-            .map((x: Song, y: number) => `**${y + 1} \`${x.info.title} [${
+            .map((x: Song, y: number) => `**${y + 1}. [${x.info.title}](${x.info.url}) [${
                 this.client.util.parseDur(x.info.duration!)
-            }]\`**`);
+            }]**`);
 
         const chunked = this.client.util.chunk(queue!, 10);
         const queueMessage = await msg.channel.send({
             embed: {
+                author: {
+                    name: msg.guild.name,
+                    // eslint-disable-next-line camelcase
+                    icon_url: msg.guild.iconURL({ dynamic: true })!
+                },
                 color: this.client.config.color,
                 description: chunked[0].join("\n"),
                 footer: {
@@ -60,21 +65,19 @@ export default class QueueCommand extends Command {
 
         collector
             .on("collect", (reaction: MessageReaction) => {
+                void reaction.users.remove(author);
                 const collect = reaction.emoji.name;
-                if (collect === this.emojis[0]) {
-                    page--;
-                    void reaction.users.remove(author);
-                }
-                if (collect === this.emojis[1]) {
-                    return collector.stop();
-                }
-                if (collect === this.emojis[2]) {
-                    page++;
-                    void reaction.users.remove(author);
-                }
+                if (collect === this.emojis[0]) page--;
+                if (collect === this.emojis[1]) return collector.stop();
+                if (collect === this.emojis[2]) page++;
                 page = ((page % chunked.length) + chunked.length) % chunked.length;
                 void msg.edit({
                     embed: {
+                        author: {
+                            name: msg.guild!.name,
+                            // eslint-disable-next-line camelcase
+                            icon_url: msg.guild!.iconURL({ dynamic: true })!
+                        },
                         color: this.client.config.color,
                         description: chunked[page].join("\n"),
                         footer: {
@@ -84,8 +87,8 @@ export default class QueueCommand extends Command {
                 });
                 void this.awaitReactions(msg, author, chunked, page);
             })
-            .on("end", () => {
-                void msg.reactions.removeAll();
+            .on("end", (_, reason) => {
+                if (["time", "user"].includes(reason)) void msg.reactions.removeAll();
             });
     }
 }
