4002052cc21b639357c92b000ab1d148c6737d02
Allvaa <alvamailindo@gmail.com>
2020-11-21 21:55:58 +0700

    :thinking:

--- src/extenders/Message.ts
+++ src/extenders/Message.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import type { MessageOptions, MessageAdditions, StringResolvable } from "discord.js";
@@ -5,32 +6,41 @@ import { APIMessage, Structures } from "discord.js";
 
 class Message extends Structures.get("Message") {
     public async inlineReply(content: StringResolvable | APIMessage, options?: MessageOptions | MessageAdditions): Promise<Message | Message[]> {
-        // @ts-expect-error 2339
-        const mention = typeof options?.mention === "undefined" ? true : options.mention;
-        let apiMessage;
+        const mentionRepliedUser = typeof (options || content)?.allowedMentions?.repliedUser === "undefined" ? true : (options || content)?.allowedMentions?.repliedUser;
+        delete (options || content)?.allowedMentions?.repliedUser;
 
-        if (content instanceof APIMessage) {
-            apiMessage = content.resolveData();
-        } else {
-            apiMessage = APIMessage.create(this.channel, content, options!).resolveData();
+        const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options!).resolveData();
+        Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
+    
+        // @ts-expect-error 2339
+        if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0)
             // @ts-expect-error 2339
-            if (Array.isArray(apiMessage.data.content)) {
-                return Promise.all(apiMessage.split().map(x => this.inlineReply(x, options)) as unknown as Message[]);
-            }
+            apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
+        // @ts-expect-error 2339
+        if (typeof apiMessage.data.allowed_mentions.replied_user === "undefined")
+            // @ts-expect-error 2339
+            Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
+
+        // @ts-expect-error 2339
+        if (Array.isArray(apiMessage.data.content)) {
+            return Promise.all(apiMessage.split().map(x => this.inlineReply(x)) as unknown as Message[]);
         }
 
         const { data, files } = await apiMessage.resolveFiles();
         // @ts-expect-error 2341
         return this.client.api.channels[this.channel.id].messages
-            .post({ data: { ...data, message_reference: { message_id: this.id }, allowed_mentions: { replied_user: mention } }, files }) // eslint-disable-line camelcase
+            .post({ data, files })
             // @ts-expect-error 2341
-            .then(d => this.client.actions.MessageCreate.handle(d).message) as Message;
+            .then(d => this.client.actions.MessageCreate.handle(d).message);
     }
 }
 
 declare module "discord.js" {
+    export interface MessageMentionOptions {
+        repliedUser: boolean;
+    }
     export interface Message {
-        inlineReply(content: StringResolvable | APIMessage, options?: MessageOptions | MessageAdditions | { mention: boolean }): Promise<Message | Message[]>;
+        inlineReply(content: StringResolvable | APIMessage, options?: MessageOptions | MessageAdditions): Promise<Message | Message[]>;
     }
 }
 
