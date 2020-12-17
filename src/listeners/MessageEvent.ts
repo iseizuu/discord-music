6f445edf2a798ce462e14a8b5f26a149016ebdb6
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-17 18:49:34 +0700

    fix(): cooldown tidak terset.

--- src/listeners/MessageEvent.ts
+++ src/listeners/MessageEvent.ts
@@ -29,7 +29,7 @@ export default class MessageEvent extends Listener {
                         return;
                     }
                 }
-                this.client.cooldowns.set(`${command.config.name} - ${msg.author.id}`, now);
+                this.client.cooldowns.set(`${command.config.name}-${msg.author.id}`, now);
                 setTimeout(() => this.client.cooldowns.delete(`${command.config.name}-${msg.author.id}`), cooldownAmount);
             }
             try {
